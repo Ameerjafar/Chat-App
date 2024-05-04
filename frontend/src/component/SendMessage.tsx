76a3e49d8ddb3ea8e7a2ecb78a454ade3386c4cf
Ameer jafar <ameerjafar123@gmail.com>
2024-05-04 10:27:32 +0530

    adding code

--- frontend/src/component/SendMessage.tsx
+++ frontend/src/component/SendMessage.tsx
@@ -7,6 +7,9 @@ interface props {
     receiverId: string,
     database: Database
 }
+type messageType = {
+    text: string
+}
 const buttonHandler = (senderId: string, receiverId: string, message: string, database: Database) => {
     console.log(receiverId);
     const messagesRef = ref(database ,'messages');
@@ -21,23 +24,44 @@ const buttonHandler = (senderId: string, receiverId: string, message: string, da
         console.log("data added successfully");
     })
 }
+
 const SendMessage: React.FC<props> = ({ senderId, receiverId, database}): JSX.Element => {
     const [message, setMessage] = useState('');
-    const [allsenderMessages, setallsenderMessages] = useState<[]>()
-    const [allreceiverMessages, setallreceiverMessages] = useState<[]>();
+    const [allsenderMessages, setallsenderMessages] = useState<messageType[]>([])
+    const [allreceiverMessages, setallreceiverMessages] = useState<messageType[]>([]);
     const gettingIndividualsMessage = () => {
         const messageref = ref(database, 'messages');
         const queryRefsender = query(messageref, 
             orderByChild("senderId"), equalTo(senderId),
-            // orderByChild('receiverId'), equalTo(receiverId)
         )
         const queryRefreceiver = query(messageref, 
             orderByChild("senderId"), equalTo(receiverId),
-            // orderByChild('receiverId'), equalTo(senderId)
         )
         get(queryRefsender).then(snapshot => {
             if(snapshot.exists()) {
-                console.log(snapshot.val());
+                snapshot.forEach(childSnapshot => {
+                    const data = childSnapshot.val();
+                    if(data.receiverId === senderId) {
+                        const message: any = data.new_message;
+                        setallsenderMessages(prev => [...prev, message])
+                        console.log(allsenderMessages)
+                    }
+                })
+            }
+            else {
+                console.log('you guys are haven"t started the legendary conversation');
+            }
+        })
+        get(queryRefreceiver).then(snapshot => {
+            if(snapshot.exists()) {
+                snapshot.forEach(childSnapshot => {
+                    const data = childSnapshot.val();
+                    if(data.senderId === receiverId) {
+                        const message: any = data.new_message;
+                        setallreceiverMessages(prev => [...prev, message]);
+                        console.log(allreceiverMessages);
+                    }
+                })
             }
             else {
                 console.log('you guys are haven"t started the legendary conversation');
